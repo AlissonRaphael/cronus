57cdb5dddb000c868a8039047ceecc354b334d45
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-03-23 21:13:49 -0300

    feat: create light theme

---
+++ src/theme/light.js
@@ -0,0 +1,19 @@
+export default {
+  Colors: {
+    Background: '#ffffff',
+
+    Primary300: '#f5f7fb',
+    Primary600: '#b3b9be',
+    Primary800: '#8a939a',
+    Primary900: '#666971',
+    Primary1000: '#ffffff',
+
+
+    Navigation: '#497db2',
+
+    Card1: '#ffecb3',
+    Card2: '#cee6e2',
+    Card3: '#ffe8e9',
+    Card4: '#b5def2',
+  }
+}
