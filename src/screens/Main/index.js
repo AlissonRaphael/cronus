9d36ea390fa5dcea6023709bdc1b668f6fc97560
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-03-26 15:14:49 -0300

    feat: create Main screen

---
+++ src/screens/Main/index.js
@@ -0,0 +1,27 @@
+import { Text } from 'react-native'
+import { Screen, Layout, Header, MainContainer, Footer } from './styles'
+
+import Button from "../../components/Button"
+import IconButton from '../../components/IconButton';
+import Timer from '../../components/Timer';
+
+export default function Main () {
+  return (
+    <Screen>
+      <Layout>
+        <Header>
+          <IconButton name="add" />
+          <IconButton name="ios-settings-sharp" />
+        </Header>
+
+        <MainContainer>
+          <Timer/>
+        </MainContainer>
+
+        <Footer>
+          <Button>Iniciar</Button>
+        </Footer>
+      </Layout>
+    </Screen>
+  )
+}
---
+++ src/screens/Main/styles.js
@@ -0,0 +1,34 @@
+import { NativeModules } from 'react-native';
+import styled from "styled-components/native"
+
+const { StatusBarManager: { HEIGHT: StatusBarHeight } } = NativeModules
+
+export const Screen = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.Colors.Background};
+`
+
+export const Layout = styled.View`
+  padding: 20px;
+  margin-top: ${() => StatusBarHeight}px;
+  flex: 1;
+`
+
+export const Header = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: flex-end;
+`
+
+export const MainContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Footer = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`
