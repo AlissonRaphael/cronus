a291ff1160c365d889bebdbcd2dce37b6b1c60b6
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-03-27 23:51:52 -0300

    refactor: clean code

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -5,40 +5,8 @@ import duration from 'dayjs/plugin/duration'
 
 dayjs.extend(duration)
 
-const ACTION = {
-  play: 'play',
-  pause: 'pause',
-  stop: 'stop',
-}
-
 export default function Timer ({}) {
-  const [time, setTime] = useState(1500000)
-  const [action, setAction] = useState(ACTION.pause)
-
-  useEffect(() => {
-    let interval;
-
-    if (action === ACTION.play) {
-      interval = setInterval(() => { setTime((state) => state - 1000) }, 1000)
-    }
-
-    if (action === ACTION.pause) {
-      clearInterval(interval)
-    }
-
-    if (action === ACTION.stop) {
-      clearInterval(interval)
-      setTime(0)
-    }
-  }, [action])
-
-  useEffect(() => {
-    if (time <= 0) {
-      setAction(ACTION.stop)
-    }
-  }, [time])
-
-  const formated = dayjs.duration(time).format('mm:ss')
+  const formated = dayjs.duration(1500000).format('mm:ss')
 
   return (
     <Container>
