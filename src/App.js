0512cc3fd02c919d3b5a9a57df765a5d0c6aae22
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-03-29 23:12:45 -0300

    feat: adds TimerProvider

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import { ThemeProvider } from 'styled-components/native';
 import light from './theme/light'
 import Main from './screens/Main'
 
+import { TimerProvider } from './Context/Timer'
+
 export default function App() {
   return (
     <ThemeProvider theme={light}>
-      <Main />
+      <TimerProvider>
+        <Main />
+      </TimerProvider>
     </ThemeProvider>
   );
 }
