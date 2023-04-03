44d41819e61b3e8a89a5ec9263d0a68585f406d9
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-04-03 20:05:45 -0300

    refactor: adds more customization support

--- src/components/IconButton/index.js
+++ src/components/IconButton/index.js
@@ -1,9 +1,10 @@
-import { Container, Icon } from './styles'
+import { Container, Icon, Text } from './styles'
 
-export default function IconButton ({ onClick, name, size, color, backgroundColor }) {
+export default function IconButton ({ children, onPress, name, size, color, backgroundColor }) {
   return (
-    <Container onClick={onClick} color={backgroundColor}>
+    <Container onPress={onPress} color={backgroundColor}>
       <Icon name={name} size={size} color={color}/>
+      { children ? <Text>{children}</Text> : null }
     </Container>
   )
 }
--- src/components/IconButton/styles.js
+++ src/components/IconButton/styles.js
@@ -4,11 +4,12 @@ import { Ionicons } from '@expo/vector-icons'
 export const Container = styled.TouchableOpacity.attrs((props) => ({
   activeOpacity: 0.5
 }))`
+  flex-direction: row;
   align-items: center;
   justify-content: center;
   padding: 16px;
   margin: 4px;
-  border-radius: 24px;
+  border-radius: 20px;
 
   background-color: ${({ theme, color }) => color || theme.Colors.Primary300};
 `
@@ -18,4 +19,10 @@ export const Icon = styled(Ionicons).attrs((props) => ({
   name: props.name
 }))`
   color: ${({ theme, color }) => color || theme.Colors.Primary800};
+`
+
+export const Text = styled.Text`
+  font-size: 14px;
+  margin-left: 8px;
+  color: ${({ theme, color }) => color || theme.Colors.Primary};
 `
