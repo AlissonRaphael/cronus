1a0095a37b19aecad207b9e500fef7279b4353e5
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-04-05 23:57:24 -0300

    feat: adds Slider input styles

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,10 +1,13 @@
-import { SliderStyles } from './styles'
+import { Container, SliderStyles, ValueStyles } from './styles'
 
-export default function Slider ({ value, onChange }) {
-  return <SliderStyles
-    onValueChange={(value) => onChange(value.toFixed(2))}
-    value={value}
-    maximumValue={50}
-    minimumValue={1}
-  />
+export default function Slider ({ value, onChange, maximumValue, minimumValue }) {
+  return <Container>
+    <SliderStyles
+      onValueChange={(value) => onChange(value.toFixed())}
+      value={value}
+      maximumValue={maximumValue || 50}
+      minimumValue={minimumValue || 1}
+    />
+    <ValueStyles>{String(value).length === 2 ? value : `0${value}`} min</ValueStyles>
+  </Container>
 }
--- src/components/Slider/styles.js
+++ src/components/Slider/styles.js
@@ -1,5 +1,12 @@
-import styled from "styled-components";
+import styled from "styled-components/native";
 import Slider from "@react-native-community/slider";
+
+export const Container = styled.View`
+  margin-top: 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
 
 export const SliderStyles = styled(Slider).attrs((props) => ({
   onValueChange: props. onValueChange,
@@ -7,5 +14,11 @@ export const SliderStyles = styled(Slider).attrs((props) => ({
   maximumValue: props.maximumValue,
   minimumValue: props.minimumValue
 }))`
+  width: 85%;
+`
 
+export const ValueStyles = styled.Text`
+  font-size: 18px;
+  color: ${({ theme }) => theme.Colors.Primary300};
+  width: 15%;
 `
